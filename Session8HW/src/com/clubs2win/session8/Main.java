a69d365a30da41e968be1936c1c43e73d5692eea
c2w-Aman <amandeepbedi01@gmail.com>
2015-07-17 18:56:37 -0700

    Homework for session 8 coding is done

--- Session8HW/src/com/clubs2win/session8/Main.java
+++ Session8HW/src/com/clubs2win/session8/Main.java
@@ -3,8 +3,59 @@ package com.clubs2win.session8;
 public class Main {
 
     public static void main(String[] args) {
-	/* this is where I will type my homework
-	
-	 */
+	// this is where I will type my homework
+        System.out.println("Is Even problem below");
+        System.out.println("6 is even. " + isEven(6));
+        System.out.println("7 is even. " + isEven(7));
+        System.out.println("0 is even. " + isEven(0));
+        System.out.println("-6 is even. " + isEven(-6));
+        System.out.println("-7 is even. " + isEven(-7));
+        System.out.println("Is Prime problem below");
+        System.out.println("6 is prime. " + isPrime(6));
+        System.out.println("2 is prime. " + isPrime(2));
+        System.out.println("25 is prime. " + isPrime(25));
+        System.out.println("9 is prime. " + isPrime(9));
+        System.out.println("100 is prime. " + isPrime(17));
+        System.out.println("Is Palindrome problem below");
+        System.out.println("123 is palindrome. " + isPalindrome(123));
+        System.out.println("121 is palindrome. " + isPalindrome(121));
+        System.out.println("-121 is palindrome. " + isPalindrome(-121));
+        System.out.println("-123 is palindrome. " + isPalindrome(-123));
+
     }
+    public static boolean isPalindrome (int n){
+        int palindrome = n; // copied number into variable
+        int reverse = 0;
+
+        while (palindrome != 0) {
+            int remainder = palindrome % 10;
+            reverse = reverse * 10 + remainder;
+            palindrome = palindrome / 10;
+        }
+
+
+        return n == reverse;
+    }
+
+    public static boolean isPrime (int n){
+        boolean prime = true;
+        if (n % 2 == 0)
+            prime = false;
+        if (n == 2)
+            prime = true;
+        for (int j = 3; j <= Math.sqrt(n); j+=2) {
+            if (n % j == 0) {
+                prime =  false;
+            }
+        }
+        return prime;
+
+
+    }
+    public static boolean isEven (int n ) {
+        boolean even;
+        even = (n / 2) * 2 == n;
+        return even;
+        }
+
 }
